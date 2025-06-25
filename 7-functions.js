7b8b7225c8eb7a612844ebc241e3ed63e325a43c
Onur Kahan <onurkahan8@gmail.com>
2025-06-25 10:53:56 +0300

    newDayNewGame

---
+++ 7-functions.js
@@ -0,0 +1,47 @@
+// let msg = 'hi'
+// function sayHello(){
+//     console.log(msg);
+// }
+// sayHello()
+
+// let fName = 'John'
+// let lName = 'Doe'
+// function printFullName(firstName, lastName) {
+//     let fName = firstName + ' ' + lastName
+//     return fName
+// }
+// let fullName = printFullName(fName, lName)
+// console.log(fullName);
+
+// function multiply(a,b){
+//     return a * b
+// }
+// let result = multiply(5, 10)
+// console.log(result);
+
+// const sayHello = function(name) {
+//     console.log('Hello ',name);
+// }
+// sayHello('John');
+
+// (function(){
+//     console.log('hey');
+
+// })()
+
+// const sayHello = (name) => {
+//     console.log('Hello', name);
+// }
+// sayHello('John');
+
+// const add = (...nums) => {
+//     let sum = 0
+//     nums.forEach(element => {
+//         sum += element
+//     }
+// )
+//     console.log('sum: ', sum);
+// }
+// add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+
---
+++ 8-objects.js
@@ -0,0 +1,26 @@
+// a = 'JavaScript';
+// b = 10
+// function printScoper(){
+//     console.log(`a: ${a}, b: ${b}`);
+
+// }
+// printScoper()
+
+// let a = 'JavaScript';
+// let b = 10;
+// function printScoper() {
+//     let a = 'Python';
+//     let b = 20;
+//     console.log(`a: ${a}, b: ${b}`);
+// }
+// printScoper(); 
+
+// const person = {
+//     firstName: 'Onur',
+//     lastName: 'Kahan',
+//     age: 22,
+//     getInfo: function(){
+//         return `My name is ${this.firstName} ${this.lastName} and I am ${this.age} years old.`;
+//     }
+// }
+// console.log(person.getInfo());
