1f5908d28ac8341abffb8e76da5f9a07849b489f
Onur Kahan <onurkahan8@gmail.com>
2025-06-26 13:44:19 +0300

    keepgoingon

---
+++ 20-cleancode.js
@@ -0,0 +1,36 @@
+// console.log(new Date().toLocaleDateString());
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(index)
+// }
+
+// let countWhile = 0
+// while (countWhile < 10) {
+//     console.log(countWhile++);
+// }
+
+// let countDoWhile = 0
+// do {
+//     console.log(countDoWhile++);
+
+// } while (countDoWhile < 10)
+
+// const cities = ['Adana', 'Ankara', 'Osmaniye', 'Istanbul']
+//cities.sort()
+// for (city of cities) {
+//     console.log(city);
+// }
+
+// cities.forEach((element) => {
+//     console.log(element);
+// })
+
+// const person = {
+//     firstName: 'Onur',
+//     lastName: 'Kahan',
+//     age: 22
+// }
+
+// for (const key in person) {
+//     console.log(`${key}: ${person[key]}`);
+// }
