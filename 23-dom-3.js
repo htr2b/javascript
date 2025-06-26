d6e6b0f409860ea3c2fcb3c4b9ab214dda5b44d5
Onur Kahan <onurkahan8@gmail.com>
2025-06-26 16:16:59 +0300

    i'll be coming home soon

---
+++ 23-dom-3.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn')
+// btn.addEventListener('click', function (e) {
+//     btn.innerHTML = 'Clicked'
+// })
+// btn.addEventListener('dblclick', function (e) {
+//     btn.innerHTML = 'Clicked'
+// })
+btn.addEventListener('mouseenter', function (e) {
+    btn.innerHTML = 'Clicked'
+})
