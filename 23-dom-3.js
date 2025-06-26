e9881d1f4f7914a351516a1fe6c3b7a095dcc822
Onur Kahan <onurkahan8@gmail.com>
2025-06-26 16:46:59 +0300

    last works

--- 23-dom-3.js
+++ 23-dom-3.js
@@ -5,6 +5,25 @@ const btn = document.getElementById('btn')
 // btn.addEventListener('dblclick', function (e) {
 //     btn.innerHTML = 'Clicked'
 // })
-btn.addEventListener('mouseenter', function (e) {
-    btn.innerHTML = 'Clicked'
+// btn.addEventListener('mouseenter', function (e) {
+//     btn.innerHTML = 'Clicked'
+// })
+
+const input = document.getElementById('name')
+let password = new Array()
+input.addEventListener('input', e => {
+    console.log('Entering input.', e.target.value);
+    password.push(e.target.value)
+})
+
+btn.addEventListener('click', e => {
+    console.log(password[password.length - 1]);
+
+})
+
+
+const colorInput = document.getElementById('colorPicker')
+colorInput.addEventListener('input', e => {
+    console.log('Input:', e.target.value);
+    document.body.style.backgroundColor = e.target.value
 })
