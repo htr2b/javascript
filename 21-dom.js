d5a58e924c2d45b7435d7ac14f32c673444fb323
Onur Kahan <onurkahan8@gmail.com>
2025-06-26 15:45:20 +0300

    little little into the middle

---
+++ 21-dom.js
@@ -0,0 +1,23 @@
+// const h1 = document.getElementsByTagName('h1')
+
+// let len = h1.length
+// for (let i = 0; i < len; i++) {
+//     console.log(h1[i]);
+
+// }
+
+// const h1 = document.getElementsByClassName('title-1')
+// console.log(h1);
+
+// const title = document.getElementById('title')
+// console.log(title);
+
+// const tagName = document.querySelectorAll('h1')
+// console.log(tagName);
+
+// const title = document.getElementById('title')
+// title.style.color = 'green'
+// title.style.backgroundColor = 'blue'
+// title.className = 'jsClass'
+
+
---
+++ 22-dom-2.js
@@ -0,0 +1,32 @@
+// let title = document.createElement('p')
+// title.className = 'created'
+// title.style.fontSize = '50px'
+// title.innerHTML = 'createdElement'
+// console.log(title);
+
+// for (let i = 0; i < 10; i++) {
+//     let newElement = document.createElement('h1')
+//     newElement.className = 'created'
+//     newElement.style.fontSize = '50px'
+//     newElement.innerHTML = `Created Element by JS, ${i}`
+//     document.body.append(newElement)
+// }
+
+// let prependElement = document.createElement('h1')
+// prependElement.className = 'prep'
+// prependElement.style.backgroundColor = 'green'
+// prependElement.innerHTML = 'Prepended element'
+// document.body.prepend(prependElement)
+
+// const appendElements = document.querySelectorAll('h1.created')
+// console.log(appendElements);
+
+// for (const element of appendElements) {
+//     element.remove()
+// }
+
+// for (const element of appendElements) {
+//     element.innerHTML = ''
+// }
+
+
